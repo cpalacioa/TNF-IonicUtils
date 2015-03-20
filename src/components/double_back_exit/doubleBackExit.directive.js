37b0b2375bc4393e54af5a9e2f4ecb95918ad510
sonicd300 <sonicdesign300@gmail.com>
2015-03-20 18:16:49 -0500

    First src push (signaturePad, doubleBackExit)

---
+++ src/components/double_back_exit/doubleBackExit.directive.js
@@ -0,0 +1,38 @@
+angular.module('TNF-IonicUtils', ['ionic'])
+  .directive('doublePressExit', function ($ionicPlatform, $timeout) {
+    return {
+      scope: {},
+      restrict: 'E',
+      template: '<div class="double-press-exit-label"><span class="label">Press Again To Exit</span></div>',
+      replace: true,
+      transclude: true,      
+      link: function($scope, iElm, iAttrs, controller) {
+        var exitApp = 0;
+
+        $ionicPlatform.registerBackButtonAction(function (e) {
+          e.preventDefault();
+
+          if (exitApp === 1) {
+            $scope.hide();
+            ionic.Platform.exitApp();
+          } else {
+            $scope.show();
+            exitApp++;
+          }
+        }, 100);
+
+        $scope.show = function () {
+          iElm.addClass('show');
+          $timeout(function(){
+            $scope.hide();            
+          }, 1500);
+        };
+
+        $scope.hide = function () {
+          iElm.removeClass('show');
+        };
+
+        $scope.hide();
+      }
+    };
+  });
